94a54be8a935f148e88a109f62c907d4044e76ab
christopherkmoore <christopherkmoore1@gmail.com>
2017-11-26 12:46:21 -0800

    Initial Commit

---
+++ panagram/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  panagram
+//
+//  Created by Christopher Moore on 11/26/17.
+//  Copyright © 2017 Christopher Moore. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
